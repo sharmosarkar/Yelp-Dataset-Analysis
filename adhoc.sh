8472a38a24395ae37d8f55e5965ca61747f1ac5a
Sharmodeep Sarkar <sharmodeepsarkar25@gmail.com>
2016-12-20 00:00:00 -0400

    update

---
+++ adhoc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILES="$(ls)"
+for f in $FILES
+do
+  echo "Processing $f file... "$f
+  # take action on each file. $f store current file name
+done
+#awk '{ sub("\r$", ""); print }' git-copy.sh > git-copy2.sh
+#mv git-copy2.sh git-copy.sh
+
+
